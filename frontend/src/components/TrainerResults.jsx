8ae8ca655395e8dda2cb28b64d78047bac1a93d0
sabina <sabina.pauric@student.um.si>
2024-11-30 19:26:30 +0100

    Implemented images random (male or female) on component TrainerResults.jsx

--- frontend/src/components/TrainerResults.jsx
+++ frontend/src/components/TrainerResults.jsx
@@ -3,6 +3,26 @@ import {Link} from "react-router-dom";
 import '../css/TrainerResult.css'
 
 const TrainerResults = ({trainers}) => {
+
+    const getRandomImage = (gender) => {
+        const femaleImages = [
+            'public/trainers/female1.jpg',
+            'public/trainers/female2.jpg',
+            'public/trainers/female3.jpg',
+        ];
+        const maleImages = [
+            'public/trainers/male1.jpg',
+            'public/trainers/male2.jpg',
+            'public/trainers/male3.jpg',
+        ];
+
+        if (gender === 'female') {
+            return femaleImages[Math.floor(Math.random() * femaleImages.length)];
+        } else if (gender === 'male') {
+            return maleImages[Math.floor(Math.random() * maleImages.length)];
+        }
+    };
+
     if (trainers.length === 0) {
         return <p>No trainers found matching the criteria.</p>;
     }
@@ -10,18 +30,31 @@ const TrainerResults = ({trainers}) => {
     return (
         <div>
             {trainers.map((trainer) => (
-                <div key={trainer._id} className="trainer-card">
-                    <h3>{trainer.name} {trainer.lastname}</h3>
-                    <div className="trainer-details">
-                        <p>Age: {trainer.age}</p>
-                        <p>City: {trainer.city}</p>
-                        <p>Postcode: {trainer.postcode}</p>
-                        <p>Gender: {trainer.gender}</p>
-
+                <div key={trainer._id} className="trainer-card2">
+                    <div className="trainer-icon2">
+                        <img
+                            src={getRandomImage(trainer.gender)}
+                            alt={`${trainer.name} ${trainer.lastname}`}
+                            className="trainer-image2"
+                        />
                     </div>
-                    <Link to={`/trainer/${trainer._id}`}>
-                        <button>Read more about {trainer.name}</button>
-                    </Link>
+                    <div className="trainer-info2">
+                        <h3>{trainer.name} {trainer.lastname}</h3>
+                        <div className="trainer-details2">
+                            <p>Age: {trainer.age}</p>
+                            <p>City: {trainer.city}</p>
+                            <p>Postcode: {trainer.postcode}</p>
+                            <p>Gender: {trainer.gender}</p>
+                            <p>
+                                {trainer.description.length > 200
+                                    ? trainer.description.substring(0, 200) + '...'
+                                    : trainer.description}
+                            </p>
+                        </div>
+                        <Link to={`/trainer/${trainer._id}`}>
+                            <button>Read more about {trainer.name}</button>
+                        </Link>
+                    </div>
                 </div>
             ))}
         </div>
