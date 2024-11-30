590dbbae5e39319ca8010c67051aa3b33832014a
sabina <sabina.pauric@student.um.si>
2024-11-30 19:26:10 +0100

    Implemented images random (male or female) on component PersonalTrainers.jsx

--- frontend/src/components/PersonalTrainers.jsx
+++ frontend/src/components/PersonalTrainers.jsx
@@ -1,7 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { FaUserAlt } from "react-icons/fa";
 import '../css/PersonalTrainers.css';
 import {Link} from "react-router-dom";
 
@@ -9,12 +8,36 @@ const PersonalTrainers = () => {
     const [trainers, setTrainers] = useState([]);
     const [isMobile, setIsMobile] = useState(false);
 
+    const getRandomImage = (gender) => {
+        const maleImages = [
+            'public/trainers/male1.jpg',
+            'public/trainers/male2.jpg',
+            'public/trainers/male3.jpg',
+        ];
+        const femaleImages = [
+            'public/trainers/female1.jpg',
+            'public/trainers/female2.jpg',
+            'public/trainers/female3.jpg',
+        ];
+
+        if (gender === 'male') {
+            return maleImages[Math.floor(Math.random() * maleImages.length)];
+        } else if (gender === 'female') {
+            return femaleImages[Math.floor(Math.random() * femaleImages.length)];
+        }
+    };
+
+
     useEffect(() => {
         const fetchTrainers = async () => {
             try {
                 const response = await axios.get('/api/trainers');
                 if (response.data && Array.isArray(response.data.data)) {
-                    setTrainers(response.data.data);
+                    const trainersWithImages = response.data.data.map((trainer) => ({
+                        ...trainer,
+                        image: getRandomImage(trainer.gender),
+                    }));
+                    setTrainers(trainersWithImages);
                 } else {
                     console.error("Expected an array of trainers, but received:", response.data);
                 }
@@ -45,8 +68,11 @@ const PersonalTrainers = () => {
                     trainers.map((trainer) => (
                         <div className="trainer-card" key={trainer._id}>
                             <div className="trainer-icon">
-                                {trainer.gender === 'female' ? <FaUserAlt className="female-icon"/> :
-                                    <FaUserAlt className="male-icon"/>}
+                                <img
+                                    src={trainer.image}
+                                    alt={`${trainer.name} ${trainer.lastname}`}
+                                    className="trainer-image"
+                                />
                             </div>
                             <div className="trainer-info">
                                 <h3>{trainer.name} {trainer.lastname}</h3>
